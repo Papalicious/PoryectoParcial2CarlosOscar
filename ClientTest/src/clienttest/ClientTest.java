f122973b95795da19bead0f498113f5d35a4db51
Jose Carlos V <jcarlosv1597@gmail.com>
2019-03-23 14:00:28 -0600

    Recibe varios mensajes
    te dice quien los mando y si eres tu o otro

--- ClientTest/src/clienttest/ClientTest.java
+++ ClientTest/src/clienttest/ClientTest.java
@@ -5,17 +5,78 @@
  */
 package clienttest;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author La Maquina del Tunas
  */
-public class ClientTest {
+public class ClientTest implements Runnable{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static String myUserName = "kudoss";
+    public static Socket socket=null;
+    public static void main(String argv[]){
+        try{
+            int character;
+            
+            //Here the connection is established with the server
+            //"127.0.0.1"
+            
+
+            
+            boolean a = true;
+             Scanner myObj = new Scanner(System.in);
+            ////////////////////////////////////////////WHILE DE ESCRITURA////////////////////////
+            while(a){
+                socket = new Socket("192.168.1.70", 8765);
+                InputStream inSocket = socket.getInputStream();
+                OutputStream outSocket = socket.getOutputStream();
+                //estructura de mensaje enviado :
+                // MENSAJE + ID DEL QUE ENVIA + IDCHAT
+
+                 String str = myObj.nextLine();
+                //String str = "Hello Server, this is a simple TCP example!";
+                str = str + "%" + "kudos"+"%" + "01" + "%"+"\n";
+                        //if el string que recibes tiene el codigo de tu chat que entre sino que se regrese a escuchar 
+                        //if el id es igual a tu que idga tu si no que diga quien es el que lo envio
+                byte buffer[] = str.getBytes();
+                outSocket.write(buffer);
+                String outPutLine ="";
+                while ((character = inSocket.read()) != -1) {
+                    outPutLine = outPutLine + (char)character;
+                    //System.out.print((char) character);
+                }
+                 //INPUT DE COMO REIBE MENSAJE
+            // ID CHAT + ID DE QUIEN ENVIO + MENSAJE 
+               // System.out.println("-------------"+outPutLine+"---------------");
+                String[] arrMsg = outPutLine.split("%");
+               // System.out.println(arrMsg[1]  + " == " + myUserName);
+                if(arrMsg[1].equals(myUserName))
+                    System.out.println("[YOU]:" + arrMsg[2]);
+                else
+                    System.out.println("["+arrMsg[1]+"]:" + arrMsg[2]);
+                socket.close();
+            }
+            //////////////////////////////////////////CIERRA WHILE ESCRITURA////////////////////////////
+           
+            socket.close();
+        }
+        catch(Exception ex){        
+            System.out.println("You must first start the server socket"); 
+            System.out.println("(YourServer.java) at the command prompt.");
+            System.out.println(ex);
+        }        
+    
+    }
+    
+    public void run(){
+        
     }
     
 }
--- ServerApp/src/serverapp/ServerApp.java
+++ ServerApp/src/serverapp/ServerApp.java
@@ -12,6 +12,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,12 +20,15 @@ import java.util.logging.Logger;
  *
  * @author La Maquina del Tunas
  */
-public class ServerApp {
+public class ServerApp implements Runnable{
 
     /**
      * @param args the command line arguments
      */
         public static InetAddress iAddress = null;
+        public ServerSocket socket;
+        public Socket insocket;
+        public String message;
 // este e sun test del commit
     public static void main(String[] args) {
         // TODO code application logic here
@@ -34,35 +38,72 @@ public class ServerApp {
             Logger.getLogger(ServerApp.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        String ipAddress = iAddress.getHostAddress();    
+        String ipAddress = iAddress.getHostAddress();  
+        System.out.println(ipAddress);
         ServerApp ssse = new ServerApp();
         ssse.execute();
     }
     
-      
+    public void putString(String message){
+        this.message=message;
+        
+    }
+    public void putinSocket(Socket insocket)
+    {
+        this.insocket = insocket;
+    }
     public void execute(){
         // TODO code application logic here
         try {    
-           ServerSocket socket = new ServerSocket(8765,50,iAddress);
+           socket = new ServerSocket(8765,50,iAddress);
 
             //Accepting the conection, it means that it is TCP
-            System.out.println("Waiting for a connection ...");
-            Socket insocket = socket.accept( );
-
-            BufferedReader in = new BufferedReader (new 
-                InputStreamReader(insocket.getInputStream()));
-            PrintWriter out = new PrintWriter (insocket.getOutputStream(), 
-                true);
+            
             boolean a = true;
             String instring="";
+            int amMsg=0;
             while(a){
-            instring = in.readLine();
-            }
-            out.println("The server got: " + instring);
+                System.out.println("Waiting for a connection ...");
+                insocket = socket.accept( );
+
+                BufferedReader in = new BufferedReader (new 
+                InputStreamReader(insocket.getInputStream()));
+                PrintWriter out = new PrintWriter (insocket.getOutputStream(), 
+                true);
+                amMsg++;
+                System.out.println(amMsg);
+                instring = in.readLine();
+                System.out.println(instring);
+                ServerApp thrd = new ServerApp();
+                thrd.putString(instring);
+                thrd.putinSocket(insocket);
+                Thread t1 = new Thread(thrd);
+                t1.start();
+                }
+            //out.println("The server got: " + instring);
             insocket.close();
         }catch (Exception e) {
             System.out.print(e);
         }
     }
+
+ public void run() {
+        
+         try {
+             BufferedReader in = new BufferedReader (new 
+                InputStreamReader(insocket.getInputStream()));
+            PrintWriter out = new PrintWriter (insocket.getOutputStream(), 
+                true);
+            String[] arrMsg = message.split("%");
+            String userId = arrMsg[1];
+            String chatid = arrMsg[2];
+            // System.out.println(message);
+              out.println(chatid+"%"+userId + "%" + arrMsg[0]);
+              Thread.sleep(500);
+            insocket.close();
+        } catch (Exception e) {
+            System.out.print(e);        }
+    }
+
     
 }
