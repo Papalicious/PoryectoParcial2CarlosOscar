38af566bd0e9682fb61f6d4830598622b055f444
Jose Carlos V <jcarlosv1597@gmail.com>
2019-03-23 10:46:03 -0600

    Server Made
    Recibe info a la computadora como server principal y la reparte

--- ServerApp/src/serverapp/ServerApp.java
+++ ServerApp/src/serverapp/ServerApp.java
@@ -5,6 +5,16 @@
  */
 package serverapp;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author La Maquina del Tunas
@@ -14,8 +24,45 @@ public class ServerApp {
     /**
      * @param args the command line arguments
      */
+        public static InetAddress iAddress = null;
+
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            iAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ServerApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String ipAddress = iAddress.getHostAddress();    
+        ServerApp ssse = new ServerApp();
+        ssse.execute();
+    }
+    
+      
+    public void execute(){
+        // TODO code application logic here
+        try {    
+           ServerSocket socket = new ServerSocket(8765,50,iAddress);
+
+            //Accepting the conection, it means that it is TCP
+            System.out.println("Waiting for a connection ...");
+            Socket insocket = socket.accept( );
+
+            BufferedReader in = new BufferedReader (new 
+                InputStreamReader(insocket.getInputStream()));
+            PrintWriter out = new PrintWriter (insocket.getOutputStream(), 
+                true);
+            boolean a = true;
+            String instring="";
+            while(a){
+            instring = in.readLine();
+            }
+            out.println("The server got: " + instring);
+            insocket.close();
+        }catch (Exception e) {
+            System.out.print(e);
+        }
     }
     
 }
